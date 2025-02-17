1f4632fb390c6e0a5260b5bc73390befdf6ff457
Ariosky Areces Gonzalez <48639831+arioskyag@users.noreply.github.com>
2025-02-17 17:57:26 +0800

    Updating

--- app/functions/xan_get.m
+++ app/functions/xan_get.m
@@ -2,6 +2,9 @@ function [output1, output2, output3]   = xan_get( varargin )
 
 if ((nargin >= 1) && ischar(varargin{1}))
     contextName = varargin{1};
+    if(nargin >= 2)
+        data = varargin{2};
+    end
 else
     return;
 end
@@ -25,8 +28,12 @@ switch contextName
         else
             output1 = [];
         end
+    case 'dataset_info'
+        datasets = xan_get('datasets');
+        output1 = datasets(find(ismember({datasets.UUID},data),1));
+        output2 = fullfile(output1.Location,'XIALPHANET.json');
     case 'datasets_file'
-        output1 = fullfile(xan_get( 'xan_dir' ),'Datasets','Datasets.json');
+        output1 = fullfile(xan_get('xan_dir'),'Datasets','Datasets.json');
     case 'test_data_url'
         output1 = 'https://github.com/brainstorm-tools/brainstorm3/raw/master/defaults/eeg';
     case 'tensor_field'
--- app/functions/xan_set.m
+++ app/functions/xan_set.m
@@ -2,30 +2,39 @@ function xan_set( varargin )
 
 if ((nargin >= 2) && ischar(varargin{1}))
     contextName = varargin{1};
-    data = varargin{2};
+    data = varargin{2};    
 else
     return
 end
 
 % Get required context structure
 switch contextName
-    case 'cfs_dir'
+    case 'xan_dir'
         
     case 'defaults_dir'
         
     case 'datasets'
-        if(~isfolder(fullfile(getUserDir(),'.CiftiStorm')))
-            mkdir(fullfile(getUserDir(),'.CiftiStorm'));
+        if(~isfolder(xan_get('xan_dir')))
+            mkdir(xan_get('xan_dir'));
         end
-        if(~isfolder(fullfile(getUserDir(),'.CiftiStorm','Datasets')))
-            mkdir(fullfile(getUserDir(),'.CiftiStorm','Datasets'));
+        if(~isfolder(xan_get('xan_db_dir')))
+            mkdir(xan_get('xan_db_dir'));
         end
-        saveJSON(data,fullfile(getUserDir(),'.CiftiStorm','Datasets','Datasets.json')) 
-        h = matlab.desktop.editor.openDocument(fullfile(getUserDir(),'.CiftiStorm','Datasets','Datasets.json'));
+        saveJSON(data,xan_get('datasets_file')) 
+        h = matlab.desktop.editor.openDocument(xan_get('datasets_file'));
         h.smartIndentContents
         h.save
         h.close
-    case 'bst_default_eeg'
+    case 'dataset_info' 
+        [~,file_name] = xan_get('dataset_info',data.UUID);
+        saveJSON(data,file_name);
+        h = matlab.desktop.editor.openDocument(file_name);
+        h.smartIndentContents
+        h.save
+        h.close
+    case 'tmp_path'
+        properties = get_properties();
+        properties.general_params.tmp.path = data;         
         
     case 'openmeeg'
         
