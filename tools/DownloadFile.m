1ff563b15981c597b44191c32825d0bada6895ad
Ariosky Areces Gonzalez <48639831+arioskyag@users.noreply.github.com>
2025-02-23 14:30:09 +0800

    updating

--- tools/DownloadFile.m
+++ tools/DownloadFile.m
@@ -1,8 +1,14 @@
-function DownloadFile(url,folder,filename)
-disp("Downloading file");
-matlab.net.http.HTTPOptions.VerifyServerName = false;
-options = weboptions('Timeout',Inf,'RequestMethod','auto');
-downladed_file = websave(fullfile(folder,filename),url,options);
-setGlobalDownloaded(true);
+function downloaded = DownloadFile(url,folder,filename)
+downloaded = false;
+try
+    disp("Downloading file");
+    matlab.net.http.HTTPOptions.VerifyServerName = false;
+    options = weboptions('Timeout',Inf,'RequestMethod','auto');
+    downladed_file = websave(fullfile(folder,filename),url,options);
+    setGlobalDownloaded(true);
+    downloaded = true;
+catch
+    downloaded = false;
+end
 end
 
